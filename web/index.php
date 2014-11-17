4a4934ccbc38e6be1cd7f298d49ef93c984e1729
Weber, Florian <fweber@ligneus.de>
2014-11-17 15:50:52 +0100

    posts

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\Finder\Finder;
+
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -10,7 +12,28 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig');
+    
+    $posts = null;
+    
+    $finder = new Finder();
+    $finder->files()->in(__DIR__ . '/posts');
+
+    foreach ($finder as $file) {
+        /*// Print the absolute path
+        print $file->getRealpath()."\n";
+
+        // Print the relative path to the file, omitting the filename
+        print $file->getRelativePath()."\n";
+
+        // Print the relative path to the file
+        print $file->getRelativePathname()."\n";*/
+        
+        $posts[] = $file->getRealpath();
+    }
+    
+    return $app['twig']->render('index.html.twig', array(
+        'posts' => $posts
+    ));
 })->bind('root');
 
 $app['debug'] = false;
