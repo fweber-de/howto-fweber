7ab0b9488d558a8dde9453cebe8dd41a2c8c253e
Weber, Florian <fweber@ligneus.de>
2014-11-18 14:53:50 +0100

    layout

---
+++ web/assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+            
+    $('.o-content img').addClass('img-responsive img-thumbnail');
+    
+    $('.o-post h1, .o-post h2, .o-post h3').anchorify({
+        text: '<i class="fa fa-link"></i>'
+    });
+
+});
